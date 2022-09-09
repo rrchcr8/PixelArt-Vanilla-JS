3813bbe9c3c90c851ccd7d441046b70a4aadba90
carlos-richter <rrch.cr8@gmail.com>
2022-09-08 23:54:22 -0400

    create initial references

---
+++ script.js
@@ -0,0 +1,12 @@
+//initial references
+let container = document.querySelector(".container");
+let gridButton = document.getElementById("submit-grid");
+let clearGridButton = document.getElementById("clear-grid");
+let gridWidth = document.getElementById("width-range");
+let gridHeight = document.getElementById("height-range");
+let colorButton = document.getElementById("color-input");
+let eraseBtn = document.getElementById("erase-btn");
+let paintBtn = document.getElementById("paint-btn");
+let widthValue = document.getElementById("wid cxth-value");
+let heightValue = document.getElementById("height-value");
+
